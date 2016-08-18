371b44df216036aeb194871ec9e8a90ebfc461a5
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-08-18 11:31:39 -0400

    saving empty js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,1 @@
 // Javascript Entry Point
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Form from './form';
-
-
-
-ReactDOM.render (
-	( <Form />
-		), document.querySelector('.app'));
